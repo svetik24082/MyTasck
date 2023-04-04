f3149632d7ed030f24bac64712ad8f6366996ca6
Света <svetik24082@mail.ru>
2023-04-04 13:33:56 +0500

    1 коммит

---
+++ src/main/java/com/example/mytasck/MyTasckApplication.java
@@ -0,0 +1,13 @@
+package com.example.mytasck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyTasckApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyTasckApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mytasck/MyTasckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mytasck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyTasckApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
