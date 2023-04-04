60b9302efb2fc854c6b73b5794e67dfae15b006a
Света <svetik24082@mail.ru>
2023-04-04 16:41:24 +0500

    2 коммит

---
+++ src/main/java/com/example/mytasck/listener/TelegramBotUpdatesListener.java
@@ -0,0 +1,51 @@
+package com.example.mytasck.listener;
+
+import com.pengrad.telegrambot.TelegramBot;
+import com.pengrad.telegrambot.UpdatesListener;
+import com.pengrad.telegrambot.model.Message;
+import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.request.SendMessage;
+import com.pengrad.telegrambot.response.SendResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+@Component
+public class TelegramBotUpdatesListener implements UpdatesListener {
+    private final Logger logger = LoggerFactory.getLogger(TelegramBotUpdatesListener.class);
+    private final TelegramBot telegramBot;
+
+    public TelegramBotUpdatesListener(TelegramBot telegramBot) {
+        this.telegramBot = telegramBot;
+    }
+    @PostConstruct
+    public void init(){
+        telegramBot.setUpdatesListener(this);
+    }
+
+    @Override
+    public int process(List<Update> updates) {
+        try {
+            updates.forEach(update -> {
+                logger.info("Handles update:{}", update);
+                Message message = update.message();
+                Long chatId = message.chat().id();
+               String text = message.text();
+               if ("/start".equals(text)) {
+                   SendMessage sendMessage = new SendMessage(chatId, "Hello");
+                  SendResponse sendResponse= telegramBot.execute(sendMessage);
+                  if (!sendResponse.isOk()){
+                      logger.error("Error during sending message : {}",sendResponse.description());
+                  }
+
+
+               }
+            });
+        }catch (Exception e){
+            logger.error(e.getMessage(),e);
+        }
+        return UpdatesListener.CONFIRMED_UPDATES_ALL;
+    }
+}
